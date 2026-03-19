297fe828edf2fc21076f629b57d321243529c7cb
Wenhao Zhang <1925005015@qq.com>
2026-03-19 15:28:34 +0800

    flask支持定义路由变量的数据类型

--- 01-introduction/flask-03-path-variable.py
+++ 01-introduction/flask-03-path-variable.py
@@ -6,6 +6,14 @@ app = Flask(__name__)
 def hello_name(name): 
     return 'Hello %s!' % name 
 
+@app.route('/blog/<int:postID>')
+def show_blog(postID): 
+    return 'Blog Number %d' % postID  
+
+@app.route('/rev/<float:revNo>')
+def revision(revNo): 
+    return 'Revision Number %f' % revNo  
+
 if __name__ == '__main__': 
     app.run(debug = True) 
  
