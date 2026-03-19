fc348bbd3e068ca1fa7af548a9d864c10d9909ab
Wenhao Zhang <1925005015@qq.com>
2026-03-19 15:36:15 +0800

    flask动态构建url并做重定向跳转

---
+++ 01-introduction/flask-04-dynamic-build-url.py
@@ -0,0 +1,25 @@
+from flask import Flask, redirect, url_for
+app = Flask(__name__)
+
+
+@app.route('/admin')  # decorator for route(argument) function
+def hello_admin():  # binding to hello_admin call
+    return 'Hello Admin'
+
+
+@app.route('/guest/<guest>')
+def hello_guest(guest):  # binding to hello_guest call
+    return 'Hello %s as Guest' % guest
+
+
+@app.route('/user/<name>')
+def hello_user(name):
+    if name == 'admin':  # dynamic binding of URL to function
+        return redirect(url_for('hello_admin'))
+    else:
+        return redirect(url_for('hello_guest', guest=name))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+ 
