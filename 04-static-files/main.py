c51c28c056b53f6c522e76214c964ed41f83d570
Wenhao Zhang <1925005015@qq.com>
2026-03-21 13:29:02 +0800

    静态文件： 引用图片

--- 04-static-files/main.py
+++ 04-static-files/main.py
@@ -9,6 +9,12 @@ def hello():
     message = "Hello, World"
     return render_template('index.html', message=message)
 
+# Images
+@app.route("/image")
+def serve_image():
+    message = "Image Route"
+    return render_template('image.html', message=message)
+
 # run the application
 if __name__ == "__main__":
     app.run(debug=True)
