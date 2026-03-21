2808b4aa9be527d8b645b3c9cdfd6db722fe73aa
Wenhao Zhang <1925005015@qq.com>
2026-03-21 11:16:36 +0800

    模版介绍： Jinja2 模版引擎使用

--- 03-templates-and-staticfiles/app.py
+++ 03-templates-and-staticfiles/app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/<name>")
+def welcome(name):
+    return render_template("welcome.html", name=name)
+
 if __name__ == "__main__":
     app.run()
